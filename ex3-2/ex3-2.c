85adf1fd9f2900dda3ae93d48e34258c930d27ef
k1910177 <k1910177@gl.cc.uec.ac.jp>
2022-11-21 17:47:43 +0900

    Fixed logical error

--- ex3-2/ex3-2.c
+++ ex3-2/ex3-2.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include "../utils/transformation.h"
+#include "../utils/constants.h"
+#include "../utils/debug.h"
+
+void invTransformOnceAndPrint(char *title, unsigned char *src, unsigned char *key) {
+    unsigned char state[Nk*Nb];
+    unsigned char w[Nk*Nb*(Nr+1)];
+
+    // Init
+    memcpy(state, src, sizeof(state));
+    KeyExpansion(w, key);
+
+    // Transform
+    invMixColumns(state, state);
+    invShiftRows(state, state);
+    invSubBytes(state, state);
+    AddRoundKey(state, state, w);
+
+    // Print
+    printAsAESState(title, state);
+}
 
 int main() {
     unsigned char PT1[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     unsigned char PT2[16] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    unsigned char Key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
 
-    invMixColumns(PT1, PT1);
-    invShiftRows(PT1, PT1);
-    invSubBytes(PT1, PT1);
-    printf("PT1: 0x");
-    for(int i = 0; i < 16; i++){
-        printf("%02x", PT1[i]);
-    }
-    printf("\n");
-
-    invMixColumns(PT2, PT2);
-    invShiftRows(PT2, PT2);
-    invSubBytes(PT2, PT2);
-    printf("PT2: 0x");
-    for(int i = 0; i < 16; i++){
-        printf("%02x", PT2[i]);
-    }
-    printf("\n");
+    invTransformOnceAndPrint("PT1", PT1, Key);
+    invTransformOnceAndPrint("PT2", PT2, Key);
 
     return 0;
 }
