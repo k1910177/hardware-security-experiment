8ac745218b3b968a2ba64a3f41c80addf9e71aed
k1910177 <k1910177@gl.cc.uec.ac.jp>
2022-11-23 18:41:56 +0900

    enchancement

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -33,22 +33,32 @@ void combinePartialKeys(unsigned char *dst, unsigned char keys[4][16]) {
 
 typedef struct arg_struct {
     unsigned char *dst;
-    unsigned char *pt1;
-    unsigned char *pt2;
+    char **files;
     int type;
+    int num;
+    int from;
+    int to;
 } arg_struct;
+
+bool done;
+const int threadCount = 8;
 
 void *findPartialKey(void *threadArg) {
     arg_struct *arg = (arg_struct *)threadArg;
 
     unsigned char key[16];
-    unsigned char state1[Nk * Nb];
-    unsigned char state2[Nk * Nb];
+    unsigned char state1[16];
+    unsigned char state2[16];
+    unsigned char pt1[arg->num][16];
+    unsigned char pt2[arg->num][16];
 
     memset(key, 0, sizeof(key));
+    for (int i = 0; i < arg->num; i++) {
+        readfile(arg->files[i], pt1[i], pt2[i]);
+    }
 
-    for (int k0 = 0; k0 < 256; k0++) {
-        printf("(type%d) %d / 255\n", arg->type, k0); // print progress
+    for (int k0 = arg->from; k0 < arg->to; k0++) {
+        printf("%d / %d\n", k0 % (256 / threadCount), (256 / threadCount));
 
         switch (arg->type) {
         case 0: key[0] = k0; break;
@@ -74,6 +84,8 @@ void *findPartialKey(void *threadArg) {
                 }
 
                 for (int k3 = 0; k3 < 256; k3++) {
+                    if (done) return NULL;
+
                     switch (arg->type) {
                     case 0: key[15] = k3; break;
                     case 1: key[3] = k3; break;
@@ -81,13 +93,20 @@ void *findPartialKey(void *threadArg) {
                     case 3: key[11] = k3; break;
                     }
 
-                    memcpy(state1, arg->pt1, sizeof(state1));
-                    memcpy(state2, arg->pt2, sizeof(state2));
+                    int i;
+                    for (i = 0; i < arg->num; i++) {
+                        memcpy(state1, pt1[i], sizeof(state1));
+                        memcpy(state2, pt2[i], sizeof(state2));
 
-                    Transform(state1, key);
-                    Transform(state2, key);
+                        Transform(state1, key);
+                        Transform(state2, key);
 
-                    if (isOneByteDiff(state1, state2, arg->type)) {
+                        if (!isOneByteDiff(state1, state2, arg->type)) {
+                            break;
+                        }
+                    }
+                    if (i == arg->num) {
+                        done = true;
                         memcpy(arg->dst, key, sizeof(key));
                         printf("type%d done!\n", arg->type);
                         printAsAESState("key", arg->dst);
@@ -100,28 +119,36 @@ void *findPartialKey(void *threadArg) {
 }
 
 int main() {
-    unsigned char PT1[4][16];
-    unsigned char PT2[4][16];
     unsigned char pKeys[4][16];
     unsigned char Key[16];
-    pthread_t threads[4];
-    arg_struct thread_params[4];
 
-    const char *files[4] = {"pt/8", "pt/10", "pt/1", "pt/4"};
+    /* clang-format off */
+    char *files[4][10] = {{"pt/8", "pt/9", "pt/12", "pt/15"},
+                          {"pt/10", "pt/14", "pt/20"},
+                          {"pt/1", "pt/2", "pt/3", "pt/11", "pt/13", "pt/17", "pt/18"},
+                          {"pt/4", "pt/5", "pt/6", "pt/7", "pt/16", "pt/19"}};
+    const int fileCounts[4] = {4, 3, 7, 6};
+    /* clang-format on */
 
-    for (int i = 0; i < 4; i++) {
-        thread_params[i].dst = pKeys[i];
-        thread_params[i].pt1 = PT1[i];
-        thread_params[i].pt2 = PT2[i];
-        thread_params[i].type = i;
+    for (int t = 0; t < 4; t++) {
+        pthread_t threads[threadCount];
+        arg_struct threadParams[threadCount];
 
-        readfile(files[i], PT1[i], PT2[i]);
-        pthread_create(&threads[i], NULL, findPartialKey,
-                       (void *)&thread_params[i]);
-    }
+        for (int i = 0; i < threadCount; i++) {
+            threadParams[i].dst = pKeys[t];
+            threadParams[i].files = files[t];
+            threadParams[i].num = fileCounts[t];
+            threadParams[i].from = i * 256 / threadCount;
+            threadParams[i].to = i * 256 / threadCount + 256 / threadCount;
+            threadParams[i].type = t;
+            pthread_create(&threads[i], NULL, findPartialKey,
+                           (void *)&threadParams[i]);
+        }
 
-    for (int i = 0; i < 4; i++) {
-        pthread_join(threads[i], NULL);
+        done = false;
+        for (int i = 0; i < threadCount; i++) {
+            pthread_join(threads[i], NULL);
+        }
     }
 
     combinePartialKeys(Key, pKeys);
