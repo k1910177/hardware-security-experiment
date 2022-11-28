c57886484f3034c4d6c613dc890c3edcaed13cf1
k1910177 <k1910177@gl.cc.uec.ac.jp>
2022-11-28 23:48:55 +0900

    ex3a

---
+++ ex3a/ex3a.c
@@ -0,0 +1,50 @@
+#include "../utils/constants.h"
+#include "../utils/debug.h"
+#include "../utils/transformation.h"
+#include <stdio.h>
+#include <string.h>
+
+void invTransformAndPrint(char *title, unsigned char *src,
+                              unsigned char *key, int num) {
+    unsigned char state[Nk * Nb];
+    unsigned char w[Nk * Nb * (Nr + 1)];
+
+    // Init
+    memcpy(state, src, sizeof(state));
+    KeyExpansion(w, key);
+
+    // Transform
+    for (int i = 0; i < num; i++) {
+        invMixColumns(state, state);
+        invShiftRows(state, state);
+        invSubBytes(state, state);
+        AddRoundKey(state, state, w + (num - i - 1) * 16);
+    }
+    // Print
+    printAsAESState(title, state);
+}
+
+int main() {
+    /* clang-format off */
+    unsigned char PT1[16] = {0x3d, 0xdd, 0x9e, 0x03, 
+                             0xc0, 0xb1, 0xa4, 0x02,
+                             0xc0, 0xff, 0xfa, 0x8b, 
+                             0xe1, 0x11, 0x58, 0x55};
+    unsigned char PT2[16] = {0x1f, 0xdd, 0x9e, 0x03, 
+                             0xc0, 0xb1, 0xa4, 0x02,
+                             0xc0, 0xff, 0xfa, 0x8b, 
+                             0xe1, 0x11, 0x58, 0x55};
+    unsigned char Key[16] = {0x00, 0x01, 0x02, 0x03, 
+                             0x04, 0x05, 0x06, 0x07,
+                             0x08, 0x09, 0x0a, 0x0b, 
+                             0x0c, 0x0d, 0x0e, 0x0f};
+    /* clang-format on */
+
+    invTransformAndPrint("PT1", PT1, Key, 1);
+    invTransformAndPrint("PT2", PT2, Key, 1);
+
+    invTransformAndPrint("PT1", PT1, Key, 2);
+    invTransformAndPrint("PT2", PT2, Key, 2);
+
+    return 0;
+}
